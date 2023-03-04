f15fda24257fbc4133312637c4f12066da291460
Sunny7313 <126257900+Sunny7313@users.noreply.github.com>
2023-03-04 07:11:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function change(){
+	document.getElementById('ham').style.display="block";
+	/*document.getElementById('menu').style.padding-top="10px";*/
+}
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
